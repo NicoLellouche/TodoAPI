cb4974b34ed44b7b48aa60612de7d75584429b60
NicoLellouche <nico.lellouche@gmail.com>
2017-09-23 14:04:40 -0400

    add delete todo

--- server.js
+++ server.js
@@ -41,6 +41,17 @@ app.post('/todos', function(req,res){
 	res.json(body);
 });
 
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no item found with that id"});
+	} else {
+		todos=_.without(todos, matchedTodo)
+		res.json(matchedTodo);
+	}
+});
+
 
 app.listen(PORT, function(){
 	console.log('express listening on port' + PORT + '!');
